4bfc4bb7bfdda9893e3f11e89174af762da67d2d
angelgoh <angelgoh2001@gmail.com>
2023-02-25 23:11:16 +0800

    Updated driver-getpoints.py to include method to get feedback_stats and renamed to driverGet.py, modified some data inputs to mock database.

--- init_db.py
+++ init_db.py
@@ -4,9 +4,9 @@ connection = sqlite3.connect('database.db')
 
 
 driverFeedback_lst = [(1,3,2,"Ninja1 was very good",1,1),
-(2,1,1,"",3,0),
-(3,1,1,"Ninja3 was very good",0,1),
-(4,2,1,"",3,1),
+(2,1,4,"",3,0),
+(3,1,4,"Ninja3 was very good",0,1),
+(4,1,3,"",3,1),
 (5,3,1,"Ninja5 was very good",2,0),
 (6,4,2,"",3,1),
 (7,5,3,"Ninja7 was very good",0,1),
