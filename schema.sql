da2ec4330bfd431ce648b29d3ceca41e71581a47
EdwardLJH <Edwardlim026@gmail.com>
2023-02-26 05:54:09 +0800

    Added orders and rejected_orders table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS driverFeedback;
 DROP TABLE IF EXISTS drivers;
-
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS rejected_orders;
 PRAGMA foreign_keys = ON;
 
 
@@ -22,3 +23,17 @@ CREATE TABLE driverFeedback (
 );
 
 
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY,
+    driver_id INTEGER NOT NULL,
+    timeslot_start DATETIME NOT NULL,
+    timeslot_end DATETIME NOT NULL,
+    attempted_time DATETIME NOT NULL,
+    FOREIGN KEY (driver_id)
+        REFERENCES drivers(driver_id)
+);
+
+CREATE TABLE rejected_orders(
+    item_name TEXT NOT NULL,
+    points INTEGER NOT NULL
+);
