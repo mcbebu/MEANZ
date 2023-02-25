c7e501394743a9ed27328117daf7e73f5b757761
EdwardLJH <Edwardlim026@gmail.com>
2023-02-26 03:35:47 +0800

    combined both backend apps

--- UserPOV.py
+++ UserPOV.py
@@ -25,20 +25,25 @@ def submitted_feedbac():
     # feedback = request.form.get('feedback')
     # tips = request.form.get('tips',0)
     # within_time = request.form.get('within_time', 0)
-    order_id = request.args.get('order_id')
-    driver_id = request.args.get('driver_id')
+    #====================================================
+    order_id = request.args.get('orderid')
+    # driver_id = request.args.get('driver_id')
     rating = request.args.get('rating')
-    feedback = request.args.get('feedback')
+    feedback = request.args.get('feedback',"")
     tips = request.args.get('tips',0)
-    within_time = request.args.get('within_time', 0)
-
-    print(order_id,driver_id,rating,feedback,tips,within_time)    
+    # within_time = request.args.get('within_time', 0)
+   
     points = 0
-    if within_time:
-        points += REWARD_POINTS
+    
+    # driver_id , within_time get from database!
 
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
+    driver_id = cur.execute("SELECT driver_id from orders WHERE order_id = ?",(order_id,)).fetchall()[0][0]
+    start,end,deliver = cur.execute("SELECT timeslot_start, timeslot_end, attempted_time from orders where order_id = ? ",(order_id,)).fetchall()[0]
+    within_time = 1 if start <= deliver <= end else 0
+    if within_time:
+        points += REWARD_POINTS
     insertion_query = '''INSERT INTO driverFeedback(order_id, driver_id, rating, feedback, tips, within_time) 
                          VALUES (?,?,?,?,?,?)'''
     row = (order_id,driver_id,rating,feedback,tips,within_time)
@@ -64,4 +69,39 @@ def hello_world():
         within_time = "Yes" if row['within_time'] != 0 else "No"
         display.append(f"<h1>{(row['order_id'],row['rating'],row['feedback'],row['tips'], within_time)}<h1>")
     return "".join(display)
+
+
+@app.route('/points', methods=["GET"])
+def get_points():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    driver_id = request.args.get("driver_id")
+
+    points = cur.execute(f'SELECT driver_id, points FROM drivers WHERE driver_id = ?',driver_id).fetchall()
+    conn.close()
+    
+    driver_id, points = points[0]
+    print(jsonify(driver_id=driver_id, points=points))
+    return jsonify(points=points)
+
+@app.route('/feedback_stats', methods=["GET"])
+def get_feedback_stats():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    driver_id = request.args.get("driver_id")
+    avg_rating = cur.execute(f'SELECT avg(rating) FROM driverFeedback WHERE driver_id = ?', driver_id).fetchall()
+    average = avg_rating[0][0]
+    
+    feedback = cur.execute(f'SELECT feedback, rating FROM driverFeedback WHERE driver_id = ? AND rating > 2 ORDER BY rating DESC LIMIT 3', driver_id).fetchall()
+    conn.close()
+
+    feedback1, rating1 = feedback[0]
+    feedback2, rating2 = feedback[1]
+    feedback3, rating3 = feedback[2]
+    feedback_array = [(feedback1, rating1), (feedback2, rating2), (feedback3, rating3)]
+
+    print(jsonify(feedback_array=feedback_array, average=average))
+    return jsonify(feedback_array=feedback_array, average=average)
+
+
 
